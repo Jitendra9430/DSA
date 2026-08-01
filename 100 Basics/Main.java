fa2023a66a4d81b34dd1ab6f15754e9c2617bee6
Delta Student <krjitendra9430@gmail.com>
2026-08-01 09:41:08 +0530

    String Completed

--- 100 Basics/Main.java
+++ 100 Basics/Main.java
@@ -96,6 +96,21 @@ public class Main{
 
     }
 
+    //Anagrams check
+    public static void checkAnagrams(String str1, String str2) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        if(str1.length() != str2.length()){
+            System.out.println("Strings are not anagrams");
+            return ;
+        }
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+    }
 
 
     public static void main(String[] args) {
